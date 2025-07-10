d4bcb77cfc52fa9f5e80b3c594d85c78505c3987
mxxikr <mxxikr@gmail.com>
2025-07-10 10:41:56 +0900

    예제 만들기 - 주문 할인 도메인 개발

---
+++ core/src/main/java/hello/core/order/Order.java
@@ -0,0 +1,46 @@
+package hello.core.order;
+
+public class Order {
+
+    private Long memberId;
+    private String itemName;
+    private int itemPrice;
+    private int discountPrice;
+
+public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+    }
+
+    public int calculatePrice() {
+        return itemPrice - discountPrice; // 할인된 가격 계산
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    @Override
+    public String toString() { // 객체 정보를 문자열로 반환
+        return "Order{" +
+                "memberId=" + memberId +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", discountPrice=" + discountPrice +
+                '}';
+    }
+}
